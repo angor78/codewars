ac2c54384790493489356ef716382212692bcd0b
Angor78 <angor78@gmail.com>
2022-04-26 13:39:31 +0300

    multiply-function

--- 17.js
+++ 17.js
@@ -1,18 +1,15 @@
 function eachCons(array, n) {
   let nArr = [];
   let tArr = [];
- 
 
-
-    for (let j = 0; j <= n - 1; j++) {
-      tArr.push(array[j]);
-  
+  for (let j = 0; j < array.length; j++) {
+    for (let k = j; k < array.length; k++) {
+      for (let i = 0; i <= n - 1; i++) {
+        tArr.push(array[i]);
+      }
+      nArr.push(tArr);
     }
-    nArr.push(tArr);
-    
-  
-
-  
+  }
 
   return nArr;
 }
