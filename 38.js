07992cce4826d772812039af26ab2ee09b66d1e5
Angor78 <angor78@gmail.com>
2022-05-07 10:52:13 +0300

    Get the Middle Character

--- 38.js
+++ 38.js
@@ -1,3 +1,11 @@
-function numberToString(num) {
-    return String(num)
+function getMiddle(s) {
+  let arr = s.split("");
+  let centerPos = Math.floor(arr.length / 2);
+  let middleCh = arr[centerPos];
+  let prevMiddleChar = arr[centerPos - 1];
+  if (arr.length % 2 === 0) {
+    return prevMiddleChar + "" + middleCh;
+  } else {
+    return middleCh;
   }
+}
