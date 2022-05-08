60d48c8c1f097400680cc8be8de534592504fed7
Angor78 <angor78@gmail.com>
2022-05-08 10:51:02 +0300

    String repeat

--- 39.js
+++ 39.js
@@ -1,7 +1,9 @@
-var summation = function (num) {
-    let sum=0
-    for(i=1; i<num+1;i++){
-      sum = sum+i
-    }
-    return sum
+function repeatStr(n, s) {
+  let newArr = s.split(" ");
+  newArr = newArr.filter((ch) => ch === " ");
+  for (i = 0; i < n - 1; i++) {
+    newArr.push(s);
   }
+  let newStr = newArr.join("");
+  return newStr;
+}
