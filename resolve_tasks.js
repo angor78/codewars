3aea354e8e265a8073406105443a21643ca6b47e
Angor78 <angor78@gmail.com>
2022-05-16 08:25:28 +0300

    Exes and Ohs

--- resolve_tasks.js
+++ resolve_tasks.js
@@ -1,4 +1,6 @@
-function invert(array) {
-  array=array.map(item=>-item)
-   return array ;
-}
+function XO(str) {
+  let array = str.split('')
+  let countO =array.filter(el=>el==='o'||el==='O')
+  let countX =array.filter(el=>el==='x'||el==='X')
+  return countX.length === countO.length 
+  }
