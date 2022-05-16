3e5c41a270b9df120ba80e1a2d11699db17ac9b0
Angor78 <angor78@gmail.com>
2022-05-16 08:13:13 +0300

    Function 1 - hello world

--- resolve_tasks.js
+++ resolve_tasks.js
@@ -1,4 +1,3 @@
-function sumTwoSmallestNumbers(numbers){  
-  numbers = numbers.sort(function(a, b){return a - b; });
-  return numbers[0] + numbers[1];
+const greet = () => {
+  return "hello world!";
 };
