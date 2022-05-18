594b922761a2cf3cf40d86c6a1109600b612704f
Angor78 <angor78@gmail.com>
2022-05-18 09:55:21 +0300

    Convert a Boolean to a String

--- resolve_tasks.js
+++ resolve_tasks.js
@@ -1,5 +1,4 @@
-String.prototype.toJadenCase = function () {
-  let arr = this.split(' ')
-  let str = arr.map((w) =>w[0].toUpperCase()+w.slice(1)).join(' ')
-   return str; 
-};
+function booleanToString(b) {
+  //your code here
+  return b ? "true" : "false";
+}
