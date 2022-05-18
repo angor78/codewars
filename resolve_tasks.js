9919663fa9082b30646ee9c9314ba0dabe6a3996
Angor78 <angor78@gmail.com>
2022-05-18 14:01:48 +0300

    Two to One

--- resolve_tasks.js
+++ resolve_tasks.js
@@ -1,4 +1,9 @@
-function booleanToString(b) {
-  //your code here
-  return b ? "true" : "false";
+function longest(s1, s2) {
+  // your code
+  let a1 = s1.split('')
+  let a2= s2.split('')
+  let array = a1.concat(a2)
+  array=array.filter((c,i)=>array.indexOf(c) === i).sort()
+  let str = array.join('')
+  return str
 }
