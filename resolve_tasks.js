4f661c178807176ba4aadbd45f7f75810fa19826
Angor78 <angor78@gmail.com>
2022-05-19 09:50:31 +0300

    fakeBinary

--- resolve_tasks.js
+++ resolve_tasks.js
@@ -1,9 +1,6 @@
-function longest(s1, s2) {
-  // your code
-  let a1 = s1.split('')
-  let a2= s2.split('')
-  let array = a1.concat(a2)
-  array=array.filter((c,i)=>array.indexOf(c) === i).sort()
-  let str = array.join('')
-  return str
+function fakeBin(x) {
+  let array = x.split("");
+  let fakeBinary = array.map((el) => (el >= 5 ? "1" : "0")).join("");
+
+  return fakeBinary;
 }
