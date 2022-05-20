c2bee06884933b7fa1e48034f50503a20005a6cf
Angor78 <angor78@gmail.com>
2022-05-20 07:53:53 +0300

    Beginner Series #1 School Paperwork

--- resolve_tasks.js
+++ resolve_tasks.js
@@ -1,11 +1,3 @@
-function nbYear(p0, percent, aug, p) {
-  let totalYears = 0;
-  let totalPopul = p0;
-  for (let i = 1; p > totalPopul; i++) {
-    let populGrowth = Math.trunc((totalPopul / 100) * percent + aug);
-    totalPopul = populGrowth + totalPopul;
-    totalYears = i;
-  }
-
-  return totalYears;
+function paperwork(n, m) {
+  return n<0||m<0?0:n*m
 }
