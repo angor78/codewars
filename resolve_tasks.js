380632b43f1986d9b3aad0806dda0e47bf6eb7b6
Angor78 <angor78@gmail.com>
2022-05-22 12:04:07 +0300

    Categorize New Member

--- resolve_tasks.js
+++ resolve_tasks.js
@@ -1,11 +1,4 @@
-function solution(number){
-  if(number<0){return 0}
-  let array=[]
-  for(i=0;i<number;i++){
-    if(i%3===0||i%5===0){
-      array.push(i)
-    }
-  }
-  let sum = array.reduce((acc,a)=>acc+a,0)
-  return sum
+function openOrSenior(data){
+  let user = data.map(u=>(u[0]>=55)&&(u[1]>7)?u='Senior':u='Open')
+  return user
 }
