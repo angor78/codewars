52120492c4d299a761b20e0ab004e1cfbf0262c1
Angor78 <angor78@gmail.com>
2022-05-23 10:15:00 +0300

    Match Ending String Patterns

--- resolve_tasks.js
+++ resolve_tasks.js
@@ -1,23 +1,6 @@
-// function findOdd(A) {
-//   A.sort();
-//   for (let i = A.length - 1; i > 0; i--) {
-//     if (A[i] == A[i - 1]) {
-//       A.splice(i - 1, 2);
-//       i -= 1;
-//     }
-//   }
-//   if (!A.length % 2 === 0) {
-//     return A[0];
-//   }
-// }
-function findOdd(A) {
-  var obj = {};
-  A.forEach(function(el){
-    obj[el] ? obj[el]++ : obj[el] = 1;
-  });
-  console.log(obj);
-  for(prop in obj) {
-    if(obj[prop] % 2 !== 0) return Number(prop);
-  }
-}
-console.log(findOdd([1,3,1,7,6,2,2,6,3,6,7]));
+let theEnding = "This is a never ending story";
+let storyRegex = /story$/;
+storyRegex.test(theEnding);
+let noEnding = "Sometimes a story will have to end";
+let result=storyRegex.test(theEnding);
+console.log(result);
