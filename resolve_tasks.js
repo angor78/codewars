21220cd1fbacd091ff12b1d9c9b41e862b8167aa
Angor78 <angor78@gmail.com>
2022-05-23 10:17:17 +0300

    Match All Letters and Numbers2

--- resolve_tasks.js
+++ resolve_tasks.js
@@ -1,6 +1,4 @@
-let theEnding = "This is a never ending story";
-let storyRegex = /story$/;
-storyRegex.test(theEnding);
-let noEnding = "Sometimes a story will have to end";
-let result=storyRegex.test(theEnding);
+let caboose = "The last car on a train is the caboose";
+let lastRegex = /caboose$/; // Change this line
+let result = lastRegex.test(caboose);
 console.log(result);
