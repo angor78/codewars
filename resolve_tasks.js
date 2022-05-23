d134cf8d393ab81747cb2225b57bb71888d7dd40
Angor78 <angor78@gmail.com>
2022-05-23 10:05:40 +0300

    Find the odd int

--- resolve_tasks.js
+++ resolve_tasks.js
@@ -1,61 +1,23 @@
-const todolist_ID1 = "43er-wesd";
-const todolist_ID2 = "43er-wess";
-
-const todoList = [
-  {
-    id: todolist_ID1,
-    title: "What to learn",
-    filter: "all",
-  },
-  {
-    id: todolist_ID2,
-    title: "What to read",
-    filter: "alctive",
-  },
-];
-
-const tasks = {
-  [todolist_ID1]: [
-    { id: 1, name: "HTML", isDone: false },
-    { id: 2, name: "CSS", isDone: true },
-    { id: 3, name: "REACT", isDone: true },
-  ],
-  [todolist_ID2]: [
-    { id: 4, name: "asd", isDone: true },
-    { id: 5, name: "HTfsdML", isDone: false },
-    { id: 6, name: "xxc", isDone: true },
-  ],
-};
-console.log(tasks[todolist_ID1][0].name);
-
-let students = [
-  {
-    name: "Bob",
-    age: 22,
-    isMarried: true,
-    scores: 85,
-  },
-  {
-    name: "Alex",
-    age: 21,
-    isMarried: true,
-    scores: 89,
-  },
-  {
-    name: "Nick",
-    age: 20,
-    isMarried: false,
-    scores: 120,
-  },
-  {
-    name: "John",
-    age: 23,
-    isMarried: false,
-    scores: 100,
-  },
-];
-
-console.log(students.reduce((acc, el) =>{
-    acc.push({...el,scores:el.scores+10})
-    return acc
-},[]));
+// function findOdd(A) {
+//   A.sort();
+//   for (let i = A.length - 1; i > 0; i--) {
+//     if (A[i] == A[i - 1]) {
+//       A.splice(i - 1, 2);
+//       i -= 1;
+//     }
+//   }
+//   if (!A.length % 2 === 0) {
+//     return A[0];
+//   }
+// }
+function findOdd(A) {
+  var obj = {};
+  A.forEach(function(el){
+    obj[el] ? obj[el]++ : obj[el] = 1;
+  });
+  console.log(obj);
+  for(prop in obj) {
+    if(obj[prop] % 2 !== 0) return Number(prop);
+  }
+}
+console.log(findOdd([1,3,1,7,6,2,2,6,3,6,7]));
