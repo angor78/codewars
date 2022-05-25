934fae48109afb4a7e4c3e46367a80c324e3b8c8
Angor78 <angor78@gmail.com>
2022-05-25 19:43:13 +0300

    arrayPlusArray

--- resolve_tasks.js
+++ resolve_tasks.js
@@ -1,15 +1,18 @@
-function digital_root(n) {
-  if (n < 9) {
-    return Number(n);
-  }
-  let numToArrStr = String(n).split("");
-  let sumOfDigit = numToArrStr.reduce((acc, el) => Number(acc) + Number(el));
-  n = sumOfDigit;
-  if (n > 9) {
-    return digital_root(n);
-  }
-  return n;
-}
+// function digital_root(n) {
+//   if (n < 9) {
+//     return Number(n);
+//   }
+//   let numToArrStr = String(n).split("");
+//   let sumOfDigit = numToArrStr.reduce((acc, el) => Number(acc) + Number(el));
+//   n = sumOfDigit;
+//   if (n > 9) {
+//     return digital_root(n);
+//   }
+//   return n;
+// }
 // function digital_root(n) {
 //   return ((n - 1) % 9) + 1;
 // }
+function arrayPlusArray(arr1, arr2) {
+  return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
+}
