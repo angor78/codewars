a3c7896a5e62f69a498878b3f4b0c5cf6ddec34c
Angor78 <angor78@gmail.com>
2022-06-11 12:37:46 +0300

    findOutlier

--- resolve_tasks.js
+++ resolve_tasks.js
@@ -207,57 +207,63 @@
 //1. Реализуйте функцию, которая принимает параметром подсторку, число повторов и разделитель, а возвращает сторку, состоящую из указанного количества повторов подстроки с использованием разделителя.
 // repeatString("yo", 3, " ") => "yo yo yo"
 // repeatString("yo", 3, ",") => "yo,yo,yo"
-function multyYo(str, n, div) {
-  let arr = [];
-  for (i = 0; i < n; i++) arr.push(str);
-  return arr.join(div);
-}
-console.log(multyYo("yo", 5, ","));
-//2. Реализуйте функцию, которая принимает параметром сторку и подстроку, а возвращает true, если строка начинается с указанной подстроки, в противном случае - false. Регистр не учитывается.
-// checkStart("Incubator", "inc") => true
-// checkStart("Incubator", "yo") => false
-function name(str, bgStr) {
-  return str.toLowerCase().startsWith(bgStr, 0);
-}
-console.log(name("Incubator", "inc"));
-console.log(name("Incubator", "yo"));
-//3. Реализуйте функцию, которая принимает параметром строку и число (количество символов), а возвращает строку из параметров, обрезанную до указанного количества символов и завершает её многоточием.
-//truncateString("Всем студентам инкубатора желаю удачи!", 10) => "Всем студе..."
-function truncateString(str, n) {
-  return str.slice(0, n) + "...";
-}
-console.log(truncateString("Всем студентам инкубатора желаю удачи!", 10));
-//4. Реализуйте функцию, которая принимает параметром сторку (предложение) и возвращает самое короткое слово в предложении, если в параметрах пустая строка, то возвращает null.
-// getMinLengthWord("Всем студентам инкубатора желаю удачи!") => "Всем"
-// getMinLengthWord("") => null
-function getMinLengthWord(str) {
-  if (str === "") null;
-  let arr = str.split(" ");
-  let sorted = arr.sort((a, b) => a.length - b.length);
-  return sorted[0];
-}
-console.log(getMinLengthWord("Всем студентам инкубатора желаю удачи!"));
+// function multyYo(str, n, div) {
+//   let arr = [];
+//   for (i = 0; i < n; i++) arr.push(str);
+//   return arr.join(div);
+// }
+// console.log(multyYo("yo", 5, ","));
+// //2. Реализуйте функцию, которая принимает параметром сторку и подстроку, а возвращает true, если строка начинается с указанной подстроки, в противном случае - false. Регистр не учитывается.
+// // checkStart("Incubator", "inc") => true
+// // checkStart("Incubator", "yo") => false
+// function name(str, bgStr) {
+//   return str.toLowerCase().startsWith(bgStr, 0);
+// }
+// console.log(name("Incubator", "inc"));
+// console.log(name("Incubator", "yo"));
+// //3. Реализуйте функцию, которая принимает параметром строку и число (количество символов), а возвращает строку из параметров, обрезанную до указанного количества символов и завершает её многоточием.
+// //truncateString("Всем студентам инкубатора желаю удачи!", 10) => "Всем студе..."
+// function truncateString(str, n) {
+//   return str.slice(0, n) + "...";
+// }
+// console.log(truncateString("Всем студентам инкубатора желаю удачи!", 10));
+// //4. Реализуйте функцию, которая принимает параметром сторку (предложение) и возвращает самое короткое слово в предложении, если в параметрах пустая строка, то возвращает null.
+// // getMinLengthWord("Всем студентам инкубатора желаю удачи!") => "Всем"
+// // getMinLengthWord("") => null
+// function getMinLengthWord(str) {
+//   if (str === "") null;
+//   let arr = str.split(" ");
+//   let sorted = arr.sort((a, b) => a.length - b.length);
+//   return sorted[0];
+// }
+// console.log(getMinLengthWord("Всем студентам инкубатора желаю удачи!"));
 
-//5. Реализуйте функцию, которая принимает параметром сторку (предложение) и возвращает то же предложение, где все слова написаны строчными, но начинаются с заглавных букв.
-// setUpperCase("всем стУдентам инкуБатора Желаю удачИ!") => "Всем Студентам Инкубатора Желаю Удачи!"
+// //5. Реализуйте функцию, которая принимает параметром сторку (предложение) и возвращает то же предложение, где все слова написаны строчными, но начинаются с заглавных букв.
+// // setUpperCase("всем стУдентам инкуБатора Желаю удачИ!") => "Всем Студентам Инкубатора Желаю Удачи!"
 
-function setUpperCase(str) {
-  str = str.split(' ').map(el=>el.toLowerCase()).join(' ')
-  return str.replace(/(^|\s)\S/g, function(a) {return a.toUpperCase()})
+// function setUpperCase(str) {
+//   str = str.split(' ').map(el=>el.toLowerCase()).join(' ')
+//   return str.replace(/(^|\s)\S/g, function(a) {return a.toUpperCase()})
+// }
+// console.log(setUpperCase("всем стУдентам инкуБатора Желаю удачИ!"));
+// //6. Реализуйте функцию, котрая принимает параметрами две строки. Если все символы второй строки содержаться в первой - возвращает true, если нет - возвращает fasle. Проверка проводится без учёта регистра.
+// // isIncludes("Incubator", "Cut") => true
+// // isIncludes("Incubator", "table") => false
+// function isIncludes(str1,str2) {
+//    let arr1=str1.toLowerCase().split('')
+//    let arr2 =str2.toLowerCase().split('')
+//    let result=[]
+//     arr1.forEach(element => {
+//     arr2.includes(element)?result.push(element):null
+//    });
+//     return  result.length===arr2.length
+//  }
+
+// console.log(isIncludes("Incubator", "Cut"));
+// console.log(isIncludes("Incubator", "table"));
+
+function findOutlier(integers){ 
+  return integers.filter(el=>el%2===0).length!==1
+    ?integers.find(el=>el%2!==0)
+    :integers.find(el=>el%2===0)
 }
-console.log(setUpperCase("всем стУдентам инкуБатора Желаю удачИ!"));
-//6. Реализуйте функцию, котрая принимает параметрами две строки. Если все символы второй строки содержаться в первой - возвращает true, если нет - возвращает fasle. Проверка проводится без учёта регистра.
-// isIncludes("Incubator", "Cut") => true
-// isIncludes("Incubator", "table") => false
-function isIncludes(str1,str2) {
-   let arr1=str1.toLowerCase().split('')
-   let arr2 =str2.toLowerCase().split('')
-   let result=[]
-    arr1.forEach(element => {
-    arr2.includes(element)?result.push(element):null
-   });
-    return  result.length===arr2.length
- }
-
-console.log(isIncludes("Incubator", "Cut"));
-console.log(isIncludes("Incubator", "table"));
